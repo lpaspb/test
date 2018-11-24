1b3b6baca7490d26dba7f4ceda53eea022553f06
Pavel Liubachev <lpaspb@MacBook-Pro-Pavel.local>
2018-11-24 20:58:09 +0300

    2

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,138 +1,163 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Panel, Cell, ListItem, List,  Button, Group, Div, Avatar, PanelHeader } from '@vkontakte/vkui';
-import Icon16Like from '@vkontakte/icons/dist/16/like';
-import Messages28 from '@vkontakte/icons/dist/28/messages';
-import icon from '../img/web-design.svg'
-const data = {
-	items: [
-		{
-		title: 'Сделай сальто',
-		status: 'accept',
-		author: 'Павел Любачев',
-		to_who: 'любому',
-		price: 1200,
-		img:'https://pp.userapi.com/c847120/v847120721/113c58/N3y7uhFJhs8.jpg',
-		time: {
-			start: '1 ноября',
-			end: '11 ноября'
-		}
-	},
-	{
-	title: 'Поцелуй книгу',
-	author: 'Тиен Мань',
-	status: 'pending',
-	to_who: 'любому',
-	price: 1200,
-	img:'https://pp.userapi.com/NOGkthIy-CqOlvspDqZxLTZq6fg8WbjyFKI00g/v85SP4U2Rng.jpg',
-	time: {
-		start: '1 ноября',
-		end: '11 ноября'
-	}
-},
-{
-title: 'Сшей кошелек',
-author: 'Григорий Любачев',
-to_who: 'любому',
-status: 'pending',
-price: 1200,
-img:'https://pp.userapi.com/c831408/v831408360/cef6f/vMVvCyvjVBo.jpg',
-time: {
-	start: '1 ноября',
-	end: '11 ноября'
-}
-},
-{
-title: 'Выйди голым на улицу',
-author: 'Любачев Павел',
-status: 'pending',
-to_who: 'любому',
-price: 1200,
-img:'https://pp.userapi.com/c847120/v847120721/113c58/N3y7uhFJhs8.jpg',
-time: {
-	start: '1 ноября',
-	end: '11 ноября'
-}
-},
-{
-title: 'Удиви меня',
-author: 'Григорий Любачев',
-status: 'vote',
-to_who: 'любому',
-price: 1200,
-img:'https://pp.userapi.com/c831408/v831408360/cef6f/vMVvCyvjVBo.jpg',
-time: {
-	start: '1 ноября',
-	end: '11 ноября'
-}
-},
-	]
-}
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import { Skeleton, Switch, Card, Icon, Avatar, Radio, Tag, Button, Row, Col, Layout } from 'antd';
+const { Header, Content, Footer } = Layout;
+
+
+const { Meta } = Card;
 
 class Home extends React.Component {
-render() {
-	return(
-	<Panel id='items_list'>
-		<Group title='Доступные задания'>
-		<List>
-		{data.items.map((item) => {
+  state = {
+    loading: true,
+  }
 
-			if(item.status == 'pending') {
-				return (
-					<Cell indicator='1000 E' before={<Avatar size={72} src={item.img}/>}>
-					<div><b>{item.title}</b></div>
-					<div style={{color:'gray', fontSize:14}}><b>{item.author}</b> бросил вызов {item.to_who}</div>
-					<Button>Принять вызов!</Button>
-				</Cell>
-			)
-		} else if (item.status == 'accept') {
-			return (
-				<Cell indicator='1000 E' before={<Avatar size={72} src={item.img}/>}>
-				<div><b>{item.title}</b></div>
-				<div style={{color:'gray', fontSize:14}}><b>Иван принял вызов {item.author}</b></div>
-				<div>Осталось 22 часа 30 минут на пруфы</div>
-			</Cell>
-		)
-		}
-		else if (item.status == 'vote') {
-			return (
-<Group>
-				<Cell indicator='1000 E' before={<Avatar size={72} src={item.img}/>}>
-				<div><b>{item.title}</b></div>
+  onChange = (checked) => {
+    this.setState({ loading: !checked });
+  }
 
-				<Cell
-	            before={<Avatar style={{ background: 'var(--destructive)' }} size={28}><Icon16Like fill="var(--white)" /></Avatar>}
-	            description="26 лайков"
-	          >
-					<div>Григорий <Button level='secondary'>Смотреть пруф</Button></div>
-				</Cell>
-				<Cell
-	            before={<Avatar style={{ background: 'var(--destructive)' }} size={28}><Icon16Like fill="var(--white)" /></Avatar>}
-	            description="1012 лайков"
-	          >
-					<div>Фам Тиен Мань <Button level='secondary'>Смотреть пруф</Button></div>
+  render() {
+    const { loading } = this.state;
 
-				</Cell>
-				<Cell
-	            before={<Avatar style={{ background: 'white' }} size={28}><Messages28/></Avatar>}
-	            description="10"
-	          >
-				</Cell>
-			</Cell>
-			</Group>
-		)
-		}
+    return (
+<Layout>
+			<Row>
+			<Col span={8}></Col>
+			      <Col span={8}>
+        <Card
 
+					style={{ width: 550, marginTop: 16 }}
+          actions={[
+					<div>
+					<Avatar style={{marginRight:10}} src="https://pp.userapi.com/NOGkthIy-CqOlvspDqZxLTZq6fg8WbjyFKI00g/v85SP4U2Rng.jpg"/>
+					<Icon type="heart" theme='filled' style={{marginRight: '5px',color:'red', fontSize:'18px', verticalAlign:'middle'}}/>
+					<span style={{color:'red'}}>2</span>
+
+					</div>,
+					<Radio.Group>
+					<Radio.Button value="large">Павел</Radio.Button>
+					<Radio.Button value="default">Тиен</Radio.Button>
+					</Radio.Group>,
+					<div>
+					<Avatar style={{marginRight:10}} src="https://pp.userapi.com/NOGkthIy-CqOlvspDqZxLTZq6fg8WbjyFKI00g/v85SP4U2Rng.jpg"/>
+					<Icon type="heart" theme='filled' style={{marginRight: '5px',color:'red', fontSize:'18px', verticalAlign:'middle'}}/>
+					<span style={{color:'red'}}>2</span>
+					</div>
+				]}
+					activeTabKey="1"
+
+        >
+          <Skeleton loading={false} avatar active>
+            <Meta
+              avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+              title="Чихни с открытыми глазами"
+              description={	<div><Tag color="gold">Выигрыш: 200 У.Е</Tag> <Tag color="green">Идет Голосование</Tag><p  style={{marginTop:'15px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p><h4 style={{marginTop:'15px'}}>Осталось 22:59</h4></div>}
+
+            />
+
+
+          </Skeleton>
+
+<video controls="controls" style={{width:'100%', marginTop:'20px'}} autoplay></video>
+        </Card>
+
+
+				<Card
+					style={{ width: 550, marginTop: 16 }}
+          actions={[
+					<div>
+					<Avatar style={{marginRight:10}} src="https://pp.userapi.com/NOGkthIy-CqOlvspDqZxLTZq6fg8WbjyFKI00g/v85SP4U2Rng.jpg"/>
+
+
+					</div>,
+					<Button type="primary">Принять вызов</Button>
+				]}
+					activeTabKey="1"
+
+        >
+          <Skeleton loading={false} avatar active>
+            <Meta
+              avatar={<Avatar src="https://pp.userapi.com/c847120/v847120721/113c58/N3y7uhFJhs8.jpg" />}
+              title="Чихни с открытыми глазами"
+              description={	<div><Tag color="gold">Выигрыш: 200 У.Е</Tag> <Tag color="blue">Поиск соперника</Tag><p style={{marginTop:'15px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p><h4 style={{marginTop:'15px'}}>Осталось 22:59</h4></div>}
+
+            />
+
+
+          </Skeleton>
+
+
+        </Card>
+				<Card
+					style={{ width: 550, marginTop: 16 }}
+          actions={[
+					<div>
+					<Avatar style={{marginRight:10}} src="https://pp.userapi.com/NOGkthIy-CqOlvspDqZxLTZq6fg8WbjyFKI00g/v85SP4U2Rng.jpg"/>
+
+
+					</div>,
+					<Avatar style={{marginRight:10}} src="https://pp.userapi.com/c847120/v847120721/113c58/N3y7uhFJhs8.jpg"/>
+				]}
+					activeTabKey="1"
+
+        >
+          <Skeleton loading={false} avatar active>
+            <Meta
+              avatar={<Avatar src="https://pp.userapi.com/c847120/v847120721/113c58/N3y7uhFJhs8.jpg" />}
+              title="Чихни с открытыми глазами"
+              description={	<div><Tag color="gold">Выигрыш: 200 У.Е</Tag> <Tag color="cyan">Заливаются пруфы</Tag><p style={{marginTop:'15px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p><h4 style={{marginTop:'15px'}}>Осталось 22:59</h4></div>}
+
+            />
+
+
+          </Skeleton>
+
+
+        </Card>
+
+				<Card
+
+					style={{ width: 550, marginTop: 16, marginBottom: 16}}
+          actions={[
+					<div>
+					<Avatar style={{marginRight:10}} src="https://pp.userapi.com/NOGkthIy-CqOlvspDqZxLTZq6fg8WbjyFKI00g/v85SP4U2Rng.jpg"/>
+					<Icon type="heart" theme='filled' style={{marginRight: '5px',color:'gold', fontSize:'18px', verticalAlign:'middle'}}/>
+					<span style={{color:'gold'}}>3</span>
+
+					</div>,
+					<Radio.Group>
+					<Radio.Button value="large">Павел</Radio.Button>
+					<Radio.Button value="default">Тиен</Radio.Button>
+					</Radio.Group>,
+					<div>
+					<Avatar style={{marginRight:10}} src="https://pp.userapi.com/c847120/v847120721/113c58/N3y7uhFJhs8.jpg"/>
+					<Icon type="heart" theme='filled' style={{marginRight: '5px',color:'red', fontSize:'18px', verticalAlign:'middle'}}/>
+					<span style={{color:'red'}}>2</span>
+					</div>
+				]}
+					activeTabKey="1"
+
+        >
+          <Skeleton loading={false} avatar active>
+            <Meta
+              avatar={<Avatar src="https://pp.userapi.com/c847120/v847120721/113c58/N3y7uhFJhs8.jpg" />}
+              title="Чихни с открытыми глазами"
+              description={	<div><Tag color="gold">Выигрыш: 200 У.Е</Tag> <Tag color="red">Завершен</Tag><p  style={{marginTop:'15px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p><h4 style={{marginTop:'15px'}}>Осталось 22:59</h4></div>}
+
+            />
+
+
+          </Skeleton>
+
+<video controls="controls" style={{width:'100%', marginTop:'20px'}} autoplay></video>
+        </Card>
+				</Col>
+				<Col span={8}></Col>
+      </Row>
+			</Layout>
+    );
+  }
 }
-		)}
-
-		</List>
-		</Group>
-	</Panel>
-)
-}
-}
-
 
 
 export default Home;
